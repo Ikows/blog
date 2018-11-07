2fa4c513a037dbe89d71562970286dfa391c6759
Nicolas Coulaud <coulauddev@gmail.com>
2018-11-07 15:23:33 +0100

    hgvkvldkdjvjfod

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @route("/category/{category}", name="category_show")
+     * @param Category $category
+     * @return Response
+     */
+    public function show(Category $category)
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category
+        ]);
+    }
+}
