1850bc29f0baa8b10e39a128f7b97901247eaf19
Nicolas Coulaud <coulauddev@gmail.com>
2018-11-13 11:48:25 +0100

    zagsvd

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,6 +7,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
+use App\Form\ArticleSearchType;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogController extends AbstractController
 {
@@ -20,7 +22,9 @@ class BlogController extends AbstractController
      */
     public function index() : Response
     {
-        $articles = $this->getDoctrine()
+
+
+        /*$articles = $this->getDoctrine()
             ->getRepository(Article::class)
             ->findAll();
 
@@ -33,7 +37,29 @@ class BlogController extends AbstractController
         return $this->render(
             'blog/index.html.twig',
             ['articles' => $articles]
+        );*/
+
+
+        $form = $this->createForm(
+            ArticleSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
         );
+
+        $form = $this->createForm(
+            ArticleSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
+
+        return $this->render(
+            'blog/index.html.twig', [
+                'articles' => $articles,
+                'form' => $form->createView(),
+            ]
+        );
+
+
     }
 
     /**
