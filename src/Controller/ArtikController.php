14478fb20ce91cf2fe254d271397f9f54aeea186
Nicolas Coulaud <coulauddev@gmail.com>
2018-11-19 11:11:10 +0100

    quete11

--- src/Controller/ArtikController.php
+++ src/Controller/ArtikController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArtikController extends AbstractController
@@ -31,6 +33,20 @@ class ArtikController extends AbstractController
             'controller_name' => 'ArtikController',
             'categories' => $categories,
             'resultats' => $resultats
+        ]);
+    }
+
+    /**
+     * @Route("article/{article}", name="article_show")
+     * @param Article $article
+     * @return Response
+     */
+    public function showArticle(Article $article): Response
+    {
+        $tags = $article->getTags();
+        return $this->render('artik/show.html.twig', [
+            'tags' => $tags,
+            'article' => $article
         ]);
     }
 }
---
+++ src/Controller/TagController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Tag;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TagController extends AbstractController
+{
+    /**
+     * @Route("/tag/{name}", name="tag")
+     */
+    public function index(Tag $tag)
+    {
+        return $this->render('tag/index.html.twig', [
+            'tag' => $tag,
+        ]);
+    }
+}
