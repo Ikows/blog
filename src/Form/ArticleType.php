f0050902ae1206b91a0cf0f24f5f7c1f9715c3cd
Nicolas Coulaud <coulauddev@gmail.com>
2018-11-20 17:18:29 +0100

    crud

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +19,6 @@ class ArticleType extends AbstractType
             ->add('content')
             ->add('slug')
             ->add('category')
-            ->add('tags')
         ;
     }
 
